faab6644e3c9c2a1223e937262c02e7f15f54b52
Jomar Júnior de Souza Pereira <jomar.junior@cern.ch>
2025-08-09 18:20:22 -0300

    Refactor main.py for improved readability and consistency in route definitions and container registrations

--- api/main.py
+++ api/main.py
@@ -3,6 +3,8 @@ from src.Core.DI.Container import Container
 from src.Core.Logging.Logger import Logger
 from dotenv import load_dotenv
 from flask import Flask
+from sqlalchemy.engine import Engine as DatabaseEngine
+from sqlalchemy import Connection as DatabaseConnection
 import sqlalchemy as sa
 
 # Load environment variables
@@ -15,48 +17,50 @@ appConfig = AppConfig.from_env()
 container = Container()
 
 # Singletons registrations
-container.register_singletons({
-    AppConfig.__name__: lambda container: appConfig,
-    sa.engine.Engine.__name__: lambda container: sa.create_engine(appConfig.database_url),
-    Logger.__name__: lambda container: Logger(appConfig.log_target)
-})
+container.register_singletons(
+    {
+        AppConfig.__name__: lambda container: appConfig,
+        DatabaseEngine.__name__: lambda container: sa.create_engine(
+            appConfig.database_url
+        ),
+        Logger.__name__: lambda container: Logger(appConfig.log_target),
+    }
+)
 
 # Factories registrations
-container.register_factories({
-    sa.Connection.__name__: lambda container: container.get(sa.engine.Engine.__name__).connect()
-})
+container.register_factories(
+    {
+        DatabaseConnection.__name__: lambda container: container.get(
+            DatabaseEngine.__name__
+        ).connect()
+    }
+)
 
 app = Flask(appConfig.app_name)
 
 # Setup Routes
-#...
+# ...
+
 
 # Index
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return {
-        "message": "Welcome to the API!"
-    }, 200
+    return {"message": "Welcome to the API!"}, 200
+
 
 # Health Check
-@app.route('/health', methods=['GET'])
+@app.route("/health", methods=["GET"])
 def health_check():
-    return {
-        "status": "healthy"
-    }, 200
+    return {"status": "healthy"}, 200
+
 
 # Catch-all route
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE"])
 def catch_all(path):
-    return {
-        "error": "Not Found"
-    }, 404
+    return {"error": "Not Found"}, 404
+
 
 # Run
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger: Logger = container.get(Logger.__name__)
-    app.run(
-        host=appConfig.host,
-        port=appConfig.port,
-        debug=appConfig.debug
-    )
+    app.run(host=appConfig.host, port=appConfig.port, debug=appConfig.debug)
