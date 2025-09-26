4b75138fbd6f4548915f6b02a6565d6d0dc1417e
JomarJunior <jomarjunior@poli.ufrj.br>
2025-09-26 15:41:48 -0300

    Wipe out everything before restarting again

--- api/src/Core/Exceptions/base.py
+++
@@ -1,8 +0,0 @@
-class BaseException(Exception):
-    """
-    Base class for all custom exceptions in the application.
-    """
-    def __init__(self, title: str, message: str, code: int = 500):
-        super().__init__(message)
-        self.title = title
-        self.code = code
